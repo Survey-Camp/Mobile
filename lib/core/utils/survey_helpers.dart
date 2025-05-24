b9a84980ebb930cb0390e8ecfa369c673bb43100
udula-g <udulaganepola@gmail.com>
2025-05-25 02:55:51 +0530

    created the SurveyCollections class and the SurveyFields class

---
+++ lib/core/utils/survey_helpers.dart
@@ -0,0 +1,29 @@
+class SurveyCollections {
+  const SurveyCollections();
+  final String survey = 'surveys';
+  final String question = 'questions';
+  final String option = 'options';
+}
+
+class SurveyFields {
+  const SurveyFields();
+  final String surveyId = 'surveyId';
+  final String title = 'title';
+  final String description = 'description';
+  final String category = 'category';
+  final String createdAt = 'createdAt';
+  final String question = 'question';
+  final String type = 'type';
+  final String required = 'required';
+  final String order = 'order';
+  final String useImages = 'useImages';
+  final String imageUrls = 'imageUrls';
+  final String optionId = 'optionId';
+  final String option = 'option';
+  final String text = 'text';
+  final String imageUrl = 'imageUrl';
+  final String minValue = 'minValue';
+  final String maxValue = 'maxValue';
+  final String minLabel = 'minLabel';
+  final String maxLabel = 'maxLabel';
+}
