30f27d2ecc70a91e4fbd3f0e9946b916121fec9d
Kisal-Mirihagoda <kisalviduranga40@gmail.com>
2025-05-28 00:22:32 +0530

    FAST APi port connected

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static const double verticalSpacing = 8.0;
+  static const double horizontalSpacing = 16.0;
+  static const double headerVerticalPadding = 24.0;
+  static const double errorFontSize = 14.0;
+  static const double circularProgressSize = 20.0;
+  static const double borderRadius = 12.0;
+  static const double shadowBlur = 4.0;
+  static const double shadowOffset = -2.0;
+}
